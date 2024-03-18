df5b1894c2f92f995f684b03e19f25cb7489db3c
Oleksandr Moshorovskyi <46052383+aleksandrmoshorovskyi@users.noreply.github.com>
2024-03-18 19:59:25 +0200

    init

---
+++ Lesson-9-class/Models.swift
@@ -0,0 +1,71 @@
+//
+//  Models.swift
+//  Lesson-9-class
+//
+//  Created by Aleksandr Moroshovskyi on 18.03.2024.
+//
+
+import Foundation
+
+class ClassA {
+    
+    var kilometers: Int = 0 {
+        
+        willSet {
+            print("kilometers will be change to \(newValue)")
+        }
+        
+        didSet {
+            print("kilometers did change to \(kilometers), olv value: \(oldValue)")
+        }
+    }
+    
+    var meters: Int {
+        
+        get {
+            return kilometers * 1000
+        }
+        
+        set {
+            kilometers = newValue / 1000
+        }
+    }
+}
+
+
+class MyBaseClass {
+
+    func doSomething() {
+        print("MyBaseClass -> doSomething")
+    }
+    
+    func doAnotherAction() -> String {
+        return "MyBaseClass"
+    }
+}
+
+class MyClassA: MyBaseClass {
+    
+    override func doSomething() {
+        super.doSomething()
+        
+        print("MyClassA -> doSomething")
+    }
+    
+    override func doAnotherAction() -> String {
+        var result = super.doAnotherAction()
+        result += " MyClassA -> doAnotherAction"
+        
+        return result
+    }
+}
+
+class MyClassB: MyClassA {
+    
+    override func doAnotherAction() -> String {
+        //var result = super.doAnotherAction()
+        let result = ", MyClassB -> doAnotherAction"
+        
+        return result
+    }
+}
--- Lesson-9-class/ViewController.swift
+++ Lesson-9-class/ViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBAction func doSomething(_ sender: Any) {
+    
+        let classA = ClassA()
+        
+        classA.meters = 12000
+        //classA.kilometers = 5
+        print(classA.meters)
+        print(classA.kilometers)
+    }
+    
+    @IBAction func doSomething2(_ sender: Any) {
+    
+        let myBaseClass = MyBaseClass()
+        myBaseClass.doSomething()
+        
+        let myClassA = MyClassA()
+        myClassA.doSomething()
+    }
+    
+    @IBAction func doSomething3(_ sender: Any) {
+        let myClassA = MyClassA()
+        print("\(myClassA.doAnotherAction())")
+        
+        let myClassB = MyClassB()
+        print("\(myClassB.doAnotherAction())")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
 
